a2d8d5ba9e41250bb7fcb6c3025ddbc3b41370ab
xcl <2496593829@qq.com>
2022-08-03 13:46:13 +0800

    购物车的展示，添加商品，删除商品功能。

--- src/test/java/com/example/weapp/WeappApplicationTests.java
+++ src/test/java/com/example/weapp/WeappApplicationTests.java
@@ -2,12 +2,22 @@ package com.example.weapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootTest
 class WeappApplicationTests {
 
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Bean
+
+	public RestTemplate init(){
+		return new RestTemplate();
 	}
 
 }
