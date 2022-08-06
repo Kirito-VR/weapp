f72968142741106bb439881989c744ab01a7031f
soft <1548138548@qq.com>
2022-08-06 10:58:24 +0800

    新增用户微信登录UserWxLoginController

---
+++ src/main/java/com/example/weapp/Controller/ReactController/UserWxLoginController.java
@@ -0,0 +1,14 @@
+package com.example.weapp.Controller.ReactController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ludh
+ * @date 2022/8/4 17:03:57
+ */
+@RestController
+@RequestMapping("")
+public class UserWxLoginController {
+
+}
